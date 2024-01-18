1fa125fd09f5f2745d6f8fb890df6569f66ddcad
roronoa-kenzo <120107803+roronoa-kenzo@users.noreply.github.com>
2024-01-18 18:03:10 +0100

    Add files via upload

---
+++ Disney + HTML CSS/data.js
@@ -0,0 +1,18 @@
+let movies = [
+    {
+        name: 'The Mandalorian',
+        des: "The Mandalorian, une épopée galactique captivante, suit le chasseur de primes Din Djarin dans l'univers Star Wars. Avec son fidèle compagnon Grogu, ils traversent des planètes lointaines, affrontent des ennemis redoutables et dévoilent les mystères de la Force, offrant une aventure inoubliable sur Disney Plus.",
+        image: 'images/scale.jpeg'
+    },
+    {
+        name: 'Loki',
+        des: "Loki, le Dieu de la Malice, se lance dans une aventure temporelle énigmatique. Suivez ses tours pendables à travers l'histoire alors qu'il affronte des adversaires redoutables et explore les secrets du multivers, offrant un voyage captivant sur Disney Plus.",
+        image: 'images/loka.jpg'
+    },
+    {
+        name: 'Wanda Vision',
+        des: "WandaVision, une série intrigante, plonge dans la vie de Wanda Maximoff et Vision, créant des réalités alternatives étonnantes. Explorez des époques et des dimensions uniques, mêlant le mystère, la comédie et l'action, pour une expérience extraordinaire sur Disney Plus.",
+        image: 'images/wanda.jpeg'
+    },
+
+]
